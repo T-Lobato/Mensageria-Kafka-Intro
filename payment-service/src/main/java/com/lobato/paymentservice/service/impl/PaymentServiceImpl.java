d814febce82858f0a7821b82387fba24bacb0e6d
Thyago Lobato <thyagollobato@gmail.com>
2023-02-02 11:28:03 -0300

    feat: add producer config

--- payment-service/src/main/java/com/lobato/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/lobato/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,27 @@ package com.lobato.paymentservice.service.impl;
 
 import com.lobato.paymentservice.model.Payment;
 import com.lobato.paymentservice.service.PaymentService;
+import java.io.Serializable;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 @Log4j2
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+        log.info("Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando o pagamento...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
